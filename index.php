b42ccda14da68406c32ca0aec2f129dff7942ff9
Abhinav Kumar <abhinavkumar08@gmail.com>
2016-07-24 01:09:03 +0530

    mail functionality14

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('phpmailer/PHPMailerAutoload.php');
 if($_POST['submit']){
 $mail = new PHPMailer(); // create a new object
 $mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
 $mail->SMTPAuth = true; // authentication enabled
 $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
 $mail->Host = "smtp.gmail.com";
