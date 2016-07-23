fc6a7f9cbd1a565d96534bbd6f2006ecd2a077b9
Abhinav Kumar <abhinavkumar08@gmail.com>
2016-07-23 23:20:36 +0530

    Adding mail functionality

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-<?php header( 'Location: ./html/Clinic.html' ) ; ?>
+<?php 
+
+#header( 'Location: ./html/Clinic.html' ) ; 
+include_once("html/Clinic.html"); 
+require_once('phpmailer/PHPMailerAutoload.php');
+
+
+echo "line8";
+$mail = new PHPMailer(); // create a new object
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587; // or 587
+$mail->IsHTML(true);
+$mail->Username = "abhinavkumar8008@gmail.com";
+$mail->Password = "Newpass19";
+$mail->SetFrom("abhinavkumar8008@gmail");
+$mail->Subject = "Test";
+$mail->Body = "hello";
+$mail->AddAddress("abhinavkumar08@gmail.com");
+
+ if(!$mail->Send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+ } else {
+	 header('Location: http://www.google.com');
+ }
+
+
+
+
+?>
